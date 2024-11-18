b107ec6c9c39d06e7d3a692d2b71f28a4b24c639
Yuvraj Pai Khot <102202216+YuvrajPaiKhot@users.noreply.github.com>
2024-11-18 08:58:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+let accessibleOptions = document.querySelector(".accessibility-options");
+let accessibleButton = document.querySelector(".accessible-button");
+
+accessibleButton.addEventListener("click",()=>{
+    accessibleOptions.classList.toggle("unhide");
+});
+
+let fontMinus = document.querySelector(".accessibility-font-size .fa-circle-minus");
+let elements = document.querySelectorAll("*:not(.accessibility-options)");
+let fontClick = 0;
+
+fontMinus.addEventListener("click", () => {
+    if(fontClick < 5){
+        fontClick+=1;
+        elements.forEach((element) => {
+            let currentFontSize = window.getComputedStyle(element).fontSize;
+            let newFontSize = parseFloat(currentFontSize) - 0.5;
+            element.style.fontSize = `${newFontSize}px`;
+            console.log(fontClick);
+        });
+    }
+});
+
+let fontPlus = document.querySelector(".accessibility-font-size .fa-circle-plus");
+
+fontPlus.addEventListener("click",()=>{
+    if(fontClick > -5){
+        fontClick-=1;
+        elements.forEach((element) => {
+            let currentFontSize = window.getComputedStyle(element).fontSize;
+            let newFontSize = parseFloat(currentFontSize) + 1;
+            element.style.fontSize = `${newFontSize}px`;
+            console.log(fontClick);
+        });
+    }
+});
+
+let reset = document.querySelector(".reset");
+
+reset.addEventListener("click",()=>{
+    elements.forEach((element)=>{
+        element.style.fontSize = "";
+    });
+    fontClick = 0;
+});
+
+
